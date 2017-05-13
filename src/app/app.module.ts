20fbfbddcd4e97bb86711f3a8a0e7004300351bc
heldermls <hmls@cin.ufpe.br>
2017-05-13 12:00:08 -0300

    Upload de fotos
    upload de fotos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,15 @@ import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 import { CadastroTutorPage} from '../pages/cadastro-tutor/cadastro-tutor';
+import { PicturePage } from '../pages/picture/picture';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { File } from '@ionic-native/file';
+import { Transfer } from '@ionic-native/transfer';
+import { FilePath } from '@ionic-native/file-path';
+import { Camera } from '@ionic-native/camera';
 
 @NgModule({
   declarations: [
@@ -19,7 +25,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     ContactPage,
     HomePage,
     TabsPage,
-    CadastroTutorPage
+    CadastroTutorPage,
+    PicturePage
   ],
   imports: [
     BrowserModule,
@@ -32,11 +39,16 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     ContactPage,
     HomePage,
     TabsPage,
-    CadastroTutorPage
+    CadastroTutorPage,
+    PicturePage
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    File,
+    Transfer,
+    Camera,
+    FilePath,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
